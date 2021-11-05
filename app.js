312dc23306b429dc1e12ad27f982ee4d8bb02aa9
yhertekin <yhertekin@gmail.com>
2021-11-05 03:57:17 +0300

    add view engine fix layout

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
 const express = require("express");
+const ejs = require("ejs");
+
 const app = express();
 
+app.set("view engine", "ejs");
+
+app.use(express.static("public"));
+
 app.get("/", (req, res) => {
-	const blog = { id: 1, title: "Blog title", description: "Blog description" };
-	res.status(200).send(blog);
+	res.render("index");
+});
+
+app.get("/about", (req, res) => {
+	res.render("about");
+});
+
+app.get("/add_post", (req, res) => {
+	res.render("add_post");
+});
+
+app.get("/post", (req, res) => {
+	res.render("post");
 });
 
 const port = 8000;

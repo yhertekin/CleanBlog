f6dbf82d6ae5793a2867040ab7d6aeda4aa7203b
yhertekin <yhertekin@gmail.com>
2021-11-07 14:34:07 +0300

    add mongoose and db add posts

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
 const express = require("express");
-const ejs = require("ejs");
+const mongoose = require("mongoose");
+// const ejs = require("ejs");
+
+const Post = require("./models/Post");
 
 const app = express();
 
+// connect db
+mongoose.connect("mongodb://localhost/cleanblog-test-db");
+
 app.set("view engine", "ejs");
 
+// middlewares
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-app.get("/", (req, res) => {
-	res.render("index");
+app.get("/", async (req, res) => {
+	const posts = await Post.find({});
+	res.render("index", { posts });
 });
 
 app.get("/about", (req, res) => {
@@ -21,6 +31,12 @@ app.get("/add_post", (req, res) => {
 
 app.get("/post", (req, res) => {
 	res.render("post");
+});
+
+app.post("/posts", async (req, res) => {
+	await Post.create(req.body);
+
+	res.redirect("/");
 });
 
 const port = 8000;

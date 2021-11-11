c5c305eec3e60e2a6f490e5c52ce15a14b166a13
yhertekin <yhertekin@gmail.com>
2021-11-11 22:21:33 +0300

    add edit && delete post

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const methodOverride = require("method-override");
 // const ejs = require("ejs");
 
 const Post = require("./models/Post");
@@ -15,6 +16,11 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(
+	methodOverride("_method", {
+		methods: ["POST", "GET"],
+	})
+);
 
 app.get("/", async (req, res) => {
 	const posts = await Post.find({});
@@ -34,9 +40,28 @@ app.get("/posts/:id", async (req, res) => {
 	res.render("post", { post });
 });
 
+app.get("/posts/edit/:id", async (req, res) => {
+	const post = await Post.findById(req.params.id);
+	res.render("edit_post", {
+		post,
+	});
+});
+
 app.post("/posts", async (req, res) => {
 	await Post.create(req.body);
+	res.redirect("/");
+});
 
+app.put("/posts/:id", async (req, res) => {
+	const post = await Post.findById(req.params.id);
+	post.title = req.body.title;
+	post.detail = req.body.detail;
+	post.save();
+	res.redirect(`/posts/${post._id}`);
+});
+
+app.delete("/posts/:id", async (req, res) => {
+	await Post.findByIdAndRemove(req.params.id);
 	res.redirect("/");
 });
 
